6297eff95695679227464dfb85639ca3da6a8ad1
Hari Surayagari <hsurayag@jbssa.com>
2024-01-03 15:12:28 -0700

    Update Vallabh's query and temp.sql

--- Vallabh's query.sql
+++ Vallabh's query.sql
@@ -20,12 +20,20 @@ SELECT
     "InventoryCasesQAStatuses_ModifiedBy",
     "InventoryCasesQAStatuses_ModifiedDate",
     "IsAvailable",
+    "WarehouseId",
+    "AreaId",
+    "WarehouseAreaDescription",
+    "WarehouseAreaIsActive",
+    "WarehouseAreaLocationDescription", 
+    "WarehouseLocationId",
     "TestId"
 FROM "PLANTFLOOR"."PLANT"."vw_InventoryDetails_sf"
 WHERE
-"IsAvailable" = 1
-    AND "QAStatusCode" = 'HLD'
-    AND "PlantCode" IN ('6010')
-    And "ProductionDate" between '2022-01-01' and '2023-12-13'; 
+     "IsAvailable" = 1
+    // AND "QAStatusCode" = 'HLD'
+    AND "PlantCode" IN ('4010')
+    //AND "ProductionDate" between '2023-12-01' and '2023-12-31';
+    //AND "AreaId" IN ('24', '25', '26', '31', '32', '6', '7');
+    AND "WarehouseAreaDescription" IN ('SOUTH FREEZER', 'SOUTH COOLER', 'SOUTH BLAST', 'SOUTH TRANSFER', 'SOUTH CVA'); 
 
 //('3500', '6005', '6010', '6015', '6020')
--- temp.sql
+++ temp.sql
@@ -1,3 +1,4 @@
+/*
 SELECT "PlantCode", "ProductionDate", "ProductId", "Shift", COUNT(*) "BoxesProduced", SUM(NetWeightCalculation) "NetWeight"
 FROM PLANTFLOOR.PLANT."vwBoxProduction_sf"
 Where "PlantCode" ='3500'
@@ -6,3 +7,15 @@ and "ProductionDate" = CURRENT_DATE()
 AND "PartFamily" = 'FAB'
 GROUP BY "PlantCode", "Shift", "ProductId", "ProductionDate";
 -- ORDER BY PlantCode, "Shift", ProductId;
+*/
+
+
+//SELECT * FROM PLANTFLOOR.PLANT."ColdScaleCarcassSides"
+//Where "KillDate" ='2023-01-02'
+// and "PlantCode" ='3015';
+
+
+//Select * From PLANTFLOOR."PLANT"."Plants";
+
+SELECT * FROM ECSBEEFPORK.ECS."LegacyOrders"
+Limit 10;
